bc27fcfadf6713e7517f62735857bad22c65fabb
Christopher Baldock <christophermbaldock@gmail.com>
2023-05-06 18:49:05 -0500

    adds animation to the graph

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import '../src/App.css';
 import {Navigation} from '../src/components/Navigation';
 import {Header} from '../src/components/Header';
 import Data from '../src/assets/data/mock-data.json';
+import AccountBalanceChart from './components/ChartComponent';
 
 function App() {
     return (
       <div className="App">
         <Navigation />
         <Header placeholder='Search Here' data={Data}/>
+        <AccountBalanceChart />
       </div>
     );
   }
