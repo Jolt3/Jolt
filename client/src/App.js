2d1e9e80737eb6344de8f553899f7d4c47e0de20
TJ Momon <tjmomon92@gmail.com>
2023-05-07 15:31:16 -0500

    created login spinner component

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import '../src/App.css';
 import {Navigation} from '../src/components/Navigation';
 import {Header} from '../src/components/Header';
 import Data from '../src/assets/data/mock-data.json';
+import {Login} from '../src/components/Login';
 
 function App() {
     return (
       <div className="App">
         <Navigation />
         <Header placeholder='Search Here' data={Data}/>
+        <Login />
       </div>
     );
   }
