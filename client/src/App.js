384a1eadb9857ffa4acaf4b3f423582f33ed0e4c
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-05-08 21:05:20 -0500

    adding apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,37 @@ import Data from '../src/assets/data/mock-data.json';
 import {Dashboard} from './components/Dashboard';
 import React, { useEffect, useContext, useCallback } from "react";
 import Context from './components/Context'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = sessionStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 
 function App() {
@@ -87,12 +118,14 @@ function App() {
   }, [dispatch, generateToken, getInfo]);
 
     return (
-      <div className="App">
-        <Navigation />
-        <Header placeholder='Search Here' data={Data}/>
-        {/* <Login /> */}
-        <Dashboard />
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <Navigation />
+          <Header placeholder='Search Here' data={Data}/>
+          {/* <Login /> */}
+          <Dashboard />
+        </div>
+      </ApolloProvider>
     );
   }
   
